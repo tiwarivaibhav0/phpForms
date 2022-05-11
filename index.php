297b56373b6602b9ac9e7026d149296c530927ff
vaibhavkumartiwari-cedcommerce <vaibhavkumartiwari@cedcoss.com>
2022-05-11 15:35:56 +0530

    Write a program for conversion

--- index.php
+++ index.php
@@ -12,23 +12,46 @@
         #btns{
             margin-left:6.9%
         }
+        #main{
+            width:40%;
+            margin:auto;
+            
+        }
+        form{
+            margin:auto;
+        }
+        button{
+            
+            width:100%;
+        }
     </style>
 </head>
 <body>
 <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST["btn"])) {
-        if(!empty($_POST["length"]))
-        $length=$_POST["length"];
+        if(!empty($_POST["conversionType"]))
+        $type=$_POST["conversionType"];
+
+        if(!empty($_POST["val"]))
+        $quantity=$_POST["val"];
         
-        if(!empty($_POST["width"]))
-        $width=$_POST["width"];
+        switch($type){
+            case "minute":
+                $res=$quantity*60;
+                $val="$quantity Hour = $res Minutes";
+                break;
+            case "second":
+                $res=$quantity*60*60;
+                $val="$quantity Hour = $res Seconds";
+                break;
+        }
         
         
        
 
-        $Area=$length*$width;
-        $peri=$length+$width;  
+        
+         
 
         
         
@@ -38,24 +61,24 @@
 
     }
     ?>
+    <div id="main">
      <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
      <table>
-    <tr><td>Length of Rectangle </td><td><input type="number" name="length" value="<?php echo(isset($length)?$length:" ");?>"></td> </tr> 
+    <tr><td><td><input type="number" name="val" value="<?php echo(isset($val)?$val:" ");?>"></td> </tr> 
     <tr></tr>
-    <tr><td>Width of Rectangle </td><td><input type="number"  name="width" value="<?php echo(isset($width)?$width:" ");?>"></td> </tr>
-    <tr></tr><tr></tr>
-    <tr><td></td><td><button type="submit" name="btn" >Calculate area & Perimeter</button></td></tr>
+     
+    <tr><td></td><td><input type="radio" name="conversionType" value="minute">hours to mins</td></tr>
+    <tr><td></td><td><input type="radio" name="conversionType" value="second">hours to seconds</td></tr>
+     <tr></tr><tr></tr>
+     <tr><td></td><td><?php echo(isset($val)?$val:" ");?></td></tr>
+     <tr></tr>
+    <tr><td></td><td><button type="submit" name="btn" >Convert</button></td></tr>
      
      </table>
      <br><br> 
-     <div id="btns">
-         <p><?php echo(isset($Area)?"Area is $Area sq m":" ");?></p>
-         <p><?php echo(isset($peri)?"Perimeter is $peri sq m":" ");?></p>
-         
      
-     </div>
     </form>
-    
+    </div>
 
 </body>
 </html>
